8122ed82f7ebd95a56c8ded79c002fae061b3ce9
Alexander List <atlex2@gmail.com>
2019-04-20 20:48:37 -0400

    validate predictions works but the predictions are bad

--- Step4_validatemodel.py
+++ Step4_validatemodel.py
@@ -5,12 +5,13 @@ from keras.models import Model, load_model
 import numpy as np
 
 models: [Model] = []
-model_names = ["./models/A_best_model.h5", "./models/B_good_model.h5"]
+model_names = ["./models/A_best_model.h5", "./models/B_good_model.h5", "./models/C_last_model.h5"]
 
 for name in model_names:
     model = load_model(name)
     models.append(model)
 
+batch_size = 32
 num_classes = 2
 
 # Data where col 0 is the data picture, and col 1 is the label
@@ -28,15 +29,20 @@ if data_set is LOAD_DFS:
 else:
     df = np.load(LOAD_CAT_BALANCED)
 
+df = df[0: len(df) - len(df) % batch_size]  # trim off non-batchable
+
 x_data = np.stack(df[:, data_column])
-df_g = df[:, gesture_classification_column]
-y_data = df_g
+y_data = df[:, gesture_classification_column]
 
 predictions = []
 for model in models:
-    prediction = model.predict(x_data)
+    prediction = model.predict(x_data, batch_size=batch_size)
     predictions.append(prediction)
 
-# for i, x in enumerate(x_data[:1000]):
-#     model_pred = [model.predict(x) for model in models]
-# print("predictions {:s} for truth: y={:s}".format(str(model_pred), y_data[i]))
+predictions = np.array(predictions)[:, 0:2000, :]
+
+for i in range(predictions.shape[1]):
+    prediction = predictions[:, i, :]
+    print("{:d}: {:s} for truth: y={:d}"
+          .format(i, str(prediction).replace("\n", "-"), y_data[i]))
+
